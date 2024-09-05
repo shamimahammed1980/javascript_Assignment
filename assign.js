e1f188718cb05f5ad9c5511384beb4858f1367d2
Shamim Ahammed <shamimahammed2k18@gmail.com>
2024-09-05 21:28:05 +0600

    javascript_Assignment1

---
+++ assign.js
@@ -0,0 +1,104 @@
+1. Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Valvo" ; 
+
+2. On one single line, declare three variables with the following names and values:
+
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+3rd Variable Name And Values Both! 
+
+let firstname = "John";  // write variable name
+let lastName = "Doe" //write variable values 
+let age = 35 ; // write variable name and values both 
+
+3. Use the correct assignment operator that will 
+result in x being 50 (same as x = x * y). 
+
+x = 10;
+y = 5;
+x *= y // what will be the value of x ? 
+
+4. Use comments to describe the correct data type of the following variables:
+
+let length = 16; // data type ? Number
+let lastName = "Johnson"; // data type ? String
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // data type ? Object
+
+5. Execute the function named myFunction.
+ 
+function myFunction() {
+ alert("Hello World!");
+}
+
+// call the function and see the output 
+myFunction() 
+
+6. 1. Create an object called person with name = John, age = 50, Then,
+access the object to alert("John is 50").
+
+const person = {
+  name: "John"
+  age: 50,
+} 
+alert(person.name + " is " + person.age);
+ 7. 1. The <button> element should do something when someone clicks on it. Try to fix it!
+
+ <button onclick="alert('Hello')">Click me.</button> 
+
+ 8. 
+
+ 1. Alert the number of items in an array, using the correct Array property: 
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+
+alert(cars.length)
+
+
+2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+  Brand[0] = "Ford"
+  console.log(Brand)
+
+
+  9.
+  
+1. Use the correct Math method to create a random number.
+
+let r = Math.random();
+
+2. Use the correct Math method to return the largest number of 10 and 20.
+
+let x = Math.max(10, 20);
+
+
+3. Use the correct Math method to get the square root of 9.
+
+let square = Math.sqrt(9);
+
+10.
+
+1. Choose the correct comparison operator to alert true, when x is greater than y. 
+x = 10;
+y = 5;
+
+alert(x > y) // comparison operator ? 
+
+
+
+2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+var voteble = (age < 18) ? "Too young" : "Old enough"; 
+alert(voteble);
+
+
+
+
